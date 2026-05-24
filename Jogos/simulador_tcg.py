f91cd30251da80017a0efb02a3d3da21d9abde17
lucas marques <74848427+LucasBorborema97@users.noreply.github.com>
2026-05-24 10:59:01 -0300

    Update simulador_tcg.py

--- Jogos/simulador_tcg.py
+++ Jogos/simulador_tcg.py
@@ -32,3 +32,32 @@ def ler_status_valido(mensagem_input):
           continue
             
         return valor
+
+# 3. Entrada de dados dos Usuários
+
+
+print("    Atributos da Carta 1   ")
+nome1 = input("Nome da Carta 1: ")
+hp1 = ler_status_valido("HP da Carta 1: ")
+ataque1 = ler_status_valido("Pontos de Ataque da Carta 1: ")
+carta1 = Carta(nome1, hp1, ataque1)
+
+print("\n   Atributos da Carta 2    ")
+nome2 = input("Nome da Carta 2: ")
+hp2 = ler_status_valido("HP da Carta 2: ")
+ataque2 = ler_status_valido("Pontos de Ataque da Carta 2: ")
+carta2 = Carta(nome2, hp2, ataque2)
+
+print("\n    A batalha irá começar!  ")
+time.sleep(1)
+
+# 4. Sorteio de quem começa
+
+
+jogadores = [carta1, carta2]
+atacante = random.choice(jogadores)
+defensor = carta2 if atacante == carta1 else carta1
+
+print(f"\n🎲 Sorteio realizado! {atacante.nome} joga primeiro.")
+print("--- A Batalha Vai Começar! ---")
+time.sleep(1)
